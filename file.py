0da8f522ac6ff5f7b7478705143ebcc5dbbbb066
travmalay <75923326+travmalay@users.noreply.github.com>
2022-11-22 09:15:26 +0100

    Update file.py

--- file.py
+++ file.py
@@ -1,33 +1,22 @@
-#importing os module 
+# Windows registry access/ (Windows Registry API) provides functions to expose the Windows registry API to Python
+from _winreg import *
 import os
 
-ProofOfSandbox = []
+browserNumber = 0
 
-VboxPathsToCheck = [r'C:\windows\System32\drivers\Vmmouse.sys', r'C:\Windows\System32\drivers\VBoxGuest.sys',
-r'C:\windows\System32\drivers\vm3dgl.dll', r'C:\windows\System32\drivers\vmdum.dll', 
-r'C:\windows\System32\drivers\vm3dver.dll', r'C:\windows\System32\drivers\vmtray.dll',
-r'C:\windows\System32\drivers\vmci.sys', r'C:\windows\System32\drivers\vmusbmouse.sys',
-r'C:\windows\System32\drivers\vmx_svga.sys', r'C:\windows\System32\drivers\vmxnet.sys',
-r'C:\windows\System32\drivers\VMToolsHook.dll', r'C:\windows\System32\drivers\vmhgfs.dll',
-r'C:\windows\System32\drivers\vmmousever.dll', r'C:\windows\System32\drivers\vmGuestLib.dll',
-r'C:\windows\System32\drivers\VmGuestLibJava.dll', r'C:\windows\System32\drivers\vmscsi.sys',
-r'C:\windows\System32\drivers\VBoxMouse.sys', r'C:\windows\System32\drivers\VBoxGuest.sys',
-r'C:\windows\System32\drivers\VBoxSF.sys', r'C:\windows\System32\drivers\VBoxVideo.sys',
-r'C:\windows\System32\vboxdisp.dll', r'C:\windows\System32\vboxhook.dll',
-r'C:\windows\System32\vboxmrxnp.dll', r'C:\windows\System32\vboxogl.dll',
-r'C:\windows\System32\vboxoglarrayspu.dll', r'C:\windows\System32\vboxoglcrutil.dll',
-r'C:\windows\System32\vboxoglerrorspu.dll', r'C:\windows\System32\vboxoglfeedbackspu.dll',
-r'C:\windows\System32\vboxoglpackspu.dll', r'C:\windows\System32\vboxoglpassthroughspu.dll',
-r'C:\windows\System32\vboxservice.exe', r'C:\windows\System32\vboxtray.exe',
-r'C:\windows\System32\VBoxControl.exe']
-#for loop to check those path once for each time
-for FilePath in VboxPathsToCheck:
-# reason to use os.path.isfile() method is to check whether the specific path is an existing file
-	if os.path.isfile(FilePath):
-    #if any file exists then added those files via append method
-		ProofOfSandbox.append(FilePath)
+browserKeys = [r'SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe', r'SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths\iexplore.exe', r'SOFTWARE\Mozilla']
 
-if ProofOfSandbox:
-	print("Vbox file exists. Do not proceed.\n{0}".format(ProofOfSandbox))
+#connecting to key in registry
+REG = ConnectRegistry(None,HKEY_LOCAL_MACHINE)
+
+for browserKey in browserKeys:
+	try:
+		Opened_Key = OpenKey(REG, browserKey)
+		browserNumber += 1
+	except:
+		pass 
+
+if browserCount >= 2:
+	print("Proceed!")
 else:
-	print("Vbox file does not exist. Proceed!")
+	print("Number of Browsers: " + str(browserNumber))
